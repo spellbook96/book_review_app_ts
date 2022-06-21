4cc4d94eaa7957837e6baf4d7ad2c0f00d4c0e93
YuLian <yulian9688@gmail.com>
2022-06-21 15:42:50 +0900

    [FIX] key error

--- src/views/sandbox/books/Books.tsx
+++ src/views/sandbox/books/Books.tsx
@@ -7,7 +7,12 @@ import { Button, Card, Col, Layout, Menu, message, Row } from "antd";
 import React, { useEffect } from "react";
 import "./css/Books.css";
 import axios from "axios";
-import { NavigateFunction, useNavigate, useParams, useSearchParams, } from "react-router-dom";
+import {
+  NavigateFunction,
+  useNavigate,
+  useParams,
+  useSearchParams,
+} from "react-router-dom";
 import store from "../../../redux/store";
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -24,45 +29,53 @@ export default function Books() {
   const [allReviewsList, setAllReviewsList] = React.useState<IReview[]>([]);
   const [isLoading, setIsLoading] = React.useState(true);
   const [currPage, setCurrPage] = React.useState(0);
-  const [searchParams,setSearchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
   // store.subscribe(() => {
   //   setCurrPage(store.getState().PaginationReducer.currPage);
   // });
   navigate = useNavigate();
   useEffect(() => {
     // console.log("change page");
-    const newPage = searchParams.has("offset")? parseInt(searchParams.get("offset")!):0
-    setCurrPage (newPage);
+    const newPage = searchParams.has("offset")
+      ? parseInt(searchParams.get("offset")!)
+      : 0;
+    setCurrPage(newPage);
     axios({
-      url: "https://api-for-missions-and-railways.herokuapp.com/books?offset="+newPage,
+      url:
+        "https://api-for-missions-and-railways.herokuapp.com/books?offset=" +
+        newPage,
       headers: {
-        "Authorization": `Bearer ${localStorage.getItem("token")}`,
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
       },
     })
       .then((res) => {
         // console.log(res.data);
-        if(res.data.length === 0){
+        if (res.data.length === 0) {
           message.error("No more reviews");
-          navigate("/books?offset="+(newPage-9));
+          navigate("/books?offset=" + (newPage - 9));
           return "ERROR";
         }
         setAllReviewsList(res.data);
         setIsLoading(false);
-        message.success("Loaded page: "+(newPage+1)+"~"+(newPage+9),0.5);
+        message.success(
+          "Loaded page: " + (newPage + 1) + "~" + (newPage + 9),
+          0.5
+        );
         return "OK";
       })
       .catch((err) => {
         message.error("Cannot connect to server", 10);
         return "ERROR";
       });
-  },[searchParams]);
+  }, [searchParams]);
 
   // const detailURL = (id: number) => {
   //   let str: string = "/detail/" + id;
   //   return <a href={str}></a>;
   // };
   const renderReviews = (data: IReview[]) => {
-    return data.map((item: IReview, index: number) => {
+    const res = data.filter((item, index) => index != 9);
+    return res.map((item: IReview, index: number) => {
       return (
         // <div className="review-item" key={index}>
         //   <div className="review-item-title">{item.title}</div>
@@ -73,23 +86,22 @@ export default function Books() {
         // </div>
         // isLoading
         ///
-        index !== 9 ? (
-          <Col span={8} key={index + data.length}>
-            <Card
-              title={"『" + item.title + "』"}
-              extra={<a href={"/detail/" + item.id}>More</a>}
-              hoverable={true}
-              onClick={() => {
-                navigate("/detail/" + item.id);
-              }}
-              style={{ width: 300, height: 120, margin: 16 }}
-              key={index + data.length * 2}>
-              <p className="review-text" key={item.id}>
-                {item.review}
-              </p>
-            </Card>
-          </Col>
-        ) : undefined
+
+        <Col span={8} key={index + data.length}>
+          <Card
+            title={"『" + item.title + "』"}
+            extra={<a href={"/detail/" + item.id}>More</a>}
+            hoverable={true}
+            onClick={() => {
+              navigate("/detail/" + item.id);
+            }}
+            style={{ width: 300, height: 120, margin: 16 }}
+            key={index + data.length * 2}>
+            <p className="review-text" key={item.id}>
+              {item.review}
+            </p>
+          </Card>
+        </Col>
       );
     });
   };
@@ -97,7 +109,7 @@ export default function Books() {
     let arr: any[] = [];
     for (let i = 0; i < n; i++) {
       arr.push(
-        <Col span={8}>
+        <Col span={8} key={i}>
           <Card
             loading={true}
             style={{ width: 300, height: 120, margin: 16 }}></Card>
@@ -113,10 +125,31 @@ export default function Books() {
         {isLoading ? makeLoadinglist(9) : renderReviews(allReviewsList)}
         {/* <Row gutter={16}>{renderReviews(allReviewsList)}</Row> */}
       </Row>
-      <div style={{ textAlign: "center" }}>Show page {(currPage+1)}-{(currPage+9)}</div>
-      <div style={{margin:"auto",width:"35%"}}>
-        <Button style={{ margin: 20 ,float:"left"}} onClick={()=>{if(currPage !== 0){setSearchParams({offset:(currPage-9)+""});setIsLoading(true)} else{message.error("Already at the top")}}}>Prev</Button>
-        <Button style={{ margin: 20,float:"right"}} onClick={()=>{setSearchParams({offset:(currPage-9)+""});setIsLoading(true);navigate("/books?offset="+(currPage+9))}}>Next</Button>
+      <div style={{ textAlign: "center" }}>
+        Show page {currPage + 1}-{currPage + 9}
+      </div>
+      <div style={{ margin: "auto", width: "35%" }}>
+        <Button
+          style={{ margin: 20, float: "left" }}
+          onClick={() => {
+            if (currPage !== 0) {
+              setSearchParams({ offset: currPage - 9 + "" });
+              setIsLoading(true);
+            } else {
+              message.error("Already at the top");
+            }
+          }}>
+          Prev
+        </Button>
+        <Button
+          style={{ margin: 20, float: "right" }}
+          onClick={() => {
+            setSearchParams({ offset: currPage - 9 + "" });
+            setIsLoading(true);
+            navigate("/books?offset=" + (currPage + 9));
+          }}>
+          Next
+        </Button>
       </div>
 
       <div></div>
